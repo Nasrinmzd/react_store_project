1b8ddae912cd559c1f3622ed2e98134ee3d69c68
Nasrin Mamizadeh <nasriiin2016@gmail.com>
2024-04-03 10:32:24 +0430

    update loing page with cookie

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { CookiesProvider } from 'react-cookie'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CookiesProvider>
     <App />
+    </CookiesProvider>
     <ToastContainer />
   </React.StrictMode>,
 )
