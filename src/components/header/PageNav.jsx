b06f091e4245ae1536e3011795f583bf2ca443ec
Nasrin Mamizadeh <nasriiin2016@gmail.com>
2024-04-06 10:10:24 +0430

    add shopping card on the nav

--- src/components/header/PageNav.jsx
+++ src/components/header/PageNav.jsx
@@ -1,13 +1,13 @@
 import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
 import Button from "../Button";
-import { SearchNormal1 } from "iconsax-react";
+import { SearchNormal1, ShoppingCart } from "iconsax-react";
 
 function PageNav() {
   return (
     <nav className="flex justify-between items-center py-5 px-6">
       <Logo />
-      <ul className="flex gap-5">
+      <ul className="flex gap-5 font-bold">
         <li>
           <NavLink to="/products">All Products</NavLink>
         </li>
@@ -18,7 +18,10 @@ function PageNav() {
           <NavLink to="/contact">Contact</NavLink>
         </li>
       </ul>
-      <div className="flex gap-3 items-center">
+      <div className="flex gap-5 items-center">
+        <div>
+          <ShoppingCart size="34" color="#ffffff" />
+        </div>
         <div className="flex items-center bg-white p-2 rounded">
           <SearchNormal1 size="18" color="#000" />
           <input className="ml-2 outline-none text-black" type="Search" placeholder="Search..." />
