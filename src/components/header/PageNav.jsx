796aaa8dcd8ed940cba4bb4deef95afa175b5373
Nasrin Mamizadeh <nasriiin2016@gmail.com>
2024-04-23 07:21:28 +0430

    Update Navbar Responsive

--- src/components/header/PageNav.jsx
+++ src/components/header/PageNav.jsx
@@ -6,9 +6,10 @@ import Search from "./Search";
 
 function PageNav({ searchTerm, setSearchTerm }) {
   return (
-    <nav className="w-full text-white flex justify-between items-center py-3 px-6 bg-black">
+    <nav className="w-full text-white bg-black py-3 px-6">
+      <div className="flex items-center justify-center md:justify-between flex-wrap gap-3">
         <Logo />
-        <ul className="flex gap-5 font-bold md:flex md:flex-row flex-col">
+        <ul className="flex gap-5 font-bold">
           <li>
             <NavLink to="/products">All Products</NavLink>
           </li>
@@ -19,14 +20,15 @@ function PageNav({ searchTerm, setSearchTerm }) {
             <NavLink to="/contact">Contact</NavLink>
           </li>
         </ul>
-      <div className="flex gap-5 items-center md:flex md:flex-row flex-col">
-        <div>
-          <ShoppingCart size="34" color="#ffffff" />
+        <div className="flex justify-center gap-2 lg:gap-5 items-center">
+          <div>
+            <ShoppingCart size="34" color="#ffffff" />
+          </div>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          <Button>
+            <NavLink to="/login">Login</NavLink>
+          </Button>
         </div>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <Button>
-          <NavLink to="/login">Login</NavLink>
-        </Button>
       </div>
     </nav>
   );
