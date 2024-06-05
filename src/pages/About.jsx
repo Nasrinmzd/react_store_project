009c061408ab7e3967905d6cc1d289bf193e116f
Nasrin Mamizadeh <nasriiin2016@gmail.com>
2024-06-05 08:08:34 +0430

    Updating width in the About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,26 @@
 function About() {
   return (
-    <div className="sm:flex items-center max-w-screen-xl mx-0 lg:mx-20">
-      <div className="sm:w-1/2 p-10">
-        <div className="image object-center text-center">
-          <img src="https://i.imgur.com/WbQnbas.png" />
+    <div className="w-[90%] mx-auto flex justify-center">
+      <div className="sm:flex items-center mx-0 lg:mx-20">
+        <div className="sm:w-1/2 p-10">
+          <div className="image object-center text-center">
+            <img src="https://i.imgur.com/WbQnbas.png" />
+          </div>
         </div>
-      </div>
-      <div className="sm:w-1/2 p-5">
-        <div className="text">
-          <span className="text-gray-500 border-b-2 border-indigo-600 uppercase">
-            About us
-          </span>
-          <h2 className="my-4 font-bold text-3xl  sm:text-4xl ">
-            About <span className="text-indigo-600">Our Company</span>
-          </h2>
-          <p className="text-gray-700">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid,
-            commodi doloremque, fugiat illum magni minus nisi nulla numquam
-            obcaecati placeat quia, repellat tempore voluptatum.
-          </p>
+        <div className="sm:w-1/2 p-5">
+          <div className="text">
+            <span className="text-gray-500 border-b-2 border-indigo-600 uppercase">
+              About us
+            </span>
+            <h2 className="my-4 font-bold text-3xl  sm:text-4xl ">
+              About <span className="text-indigo-600">Our Company</span>
+            </h2>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid,
+              commodi doloremque, fugiat illum magni minus nisi nulla numquam
+              obcaecati placeat quia, repellat tempore voluptatum.
+            </p>
+          </div>
         </div>
       </div>
     </div>
