391a6106db350884b815f9de1101d23b45fa5efd
Nasrin Mamizadeh <nasriiin2016@gmail.com>
2024-06-29 01:35:49 +0430

    Update ProtectedRoute Component

--- src/service/ProtectedRoute.jsx
+++ src/service/ProtectedRoute.jsx
@@ -3,7 +3,6 @@ import { Navigate, Outlet } from "react-router-dom";
 
 function ProtectedRoute() {
   const [cookies] = useCookies(["authToken"]);
-  console.log("ProtectedRoute is running, authToken:", cookies.authToken);
 
   if (!cookies.authToken) {
     return <Navigate to="/login" replace />;
