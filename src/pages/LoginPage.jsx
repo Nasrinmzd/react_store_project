3301d7c1edda48c1fda54d06842b1f43bbb86686
Nasrin Mamizadeh <nasriiin2016@gmail.com>
2024-07-10 09:46:16 +0430

    Update width of the Login Page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -17,7 +17,7 @@ function LoginPage() {
 
   return (
     <section className="flex items-center justify-center min-h-screen bg-indigo-800">
-      <div className="m-auto max-w-[50%] sm:h-screen md:h-3/4 bg-white rounded-xl flex flex-col-reverse sm:flex-col md:flex-row justify-between truncate ">
+      <div className="m-auto max-w-[80%] lg:max-w-[50%] sm:h-screen md:h-3/4 bg-white rounded-xl flex flex-col-reverse sm:flex-col md:flex-row justify-between truncate">
         <div className="flex flex-col flex-1 items-center bg-white m-2">
           <div className="mt-6">
             <Sun1 size="75" color="#4f46e5" variant="Bold" />
